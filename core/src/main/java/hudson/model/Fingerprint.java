2200f0afee38d63d7bbaab8b8bcbe758ffc28c9d
acontes <arnaud.contes@amadeus.com>
2014-05-09 10:30:28 +0200

    add cleaning of in-memory fingerprints

--- core/src/main/java/hudson/model/Fingerprint.java
+++ core/src/main/java/hudson/model/Fingerprint.java
@@ -1,18 +1,18 @@
 /*
  * The MIT License
- * 
+ *
  * Copyright (c) 2004-2009, Sun Microsystems, Inc., Kohsuke Kawaguchi, Yahoo! Inc.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -82,6 +82,9 @@ import org.xmlpull.v1.XmlPullParserException;
  */
 @ExportedBean
 public class Fingerprint implements ModelObject, Saveable {
+
+    public static volatile boolean enable = true;
+
     /**
      * Pointer to a {@link Build}.
      */
@@ -114,7 +117,7 @@ public class Fingerprint implements ModelObject, Saveable {
         void setName(String newName) {
             name = newName;
         }
-        
+
         /**
          * Gets the {@link Job} that this pointer points to,
          * or null if such a job no longer exists.
@@ -168,7 +171,7 @@ public class Fingerprint implements ModelObject, Saveable {
          *
          * <p>
          * This is useful to check if an artifact in MavenModule
-         * belongs to MavenModuleSet. 
+         * belongs to MavenModuleSet.
          */
         public boolean belongsTo(Job job) {
             Item p = Jenkins.getInstance().getItemByFullName(name);
@@ -750,7 +753,7 @@ public class Fingerprint implements ModelObject, Saveable {
     }
 
     private static final DateConverter DATE_CONVERTER = new DateConverter();
-    
+
     private final Date timestamp;
 
     /**
@@ -902,8 +905,10 @@ public class Fingerprint implements ModelObject, Saveable {
      * Records that a build of a job has used this file.
      */
     public synchronized void add(String jobFullName, int n) throws IOException {
+      if (enable) {
         addWithoutSaving(jobFullName, n);
         save();
+      }
     }
 
     void addWithoutSaving(String jobFullName, int n) {
@@ -1167,7 +1172,7 @@ public class Fingerprint implements ModelObject, Saveable {
                 touched = true;
             }
         }
-        
+
         if (usages != null) {
             RangeSet r = usages.get(oldName);
             if (r != null) {
@@ -1176,12 +1181,12 @@ public class Fingerprint implements ModelObject, Saveable {
                 touched = true;
             }
         }
-        
+
         if (touched) {
             save();
         }
     }
-    
+
     public Api getApi() {
         return new Api(this);
     }
